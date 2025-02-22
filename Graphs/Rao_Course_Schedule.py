ffbbf61a0461eba3d4d3d5e9f379afa78af5339a
Robert Orfao <robertorfao7@gmail.com>
2025-02-21 22:25:25 -0500

    Added Rao_Course_Schedule.py Solution problem

---
+++ Graphs/Rao_Course_Schedule.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Map each course to its prerequisites
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        # Store all courses in the current DFS path
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
+                return False # Cycle detected
+            if preMap[crs] == []:
+                return True
+
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
