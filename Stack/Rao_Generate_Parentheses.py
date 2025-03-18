2f7fbf2ae7baca1608e628087c436b2002f4b43a
Robert Orfao <robertorfao7@gmail.com>
2025-03-18 09:08:02 -0400

    Rao Generate Parentheses Solution added to Stack folder

---
+++ Stack/Rao_Generate_Parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        stack = []
+        res = []
+
+        def backtracking(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backtracking(openN + 1, closedN)
+                stack.pop()
+            
+            if closedN < openN:
+                stack.append(")")
+                backtracking(openN, closedN + 1)
+                stack.pop()
+        
+        backtracking(0, 0)
+        return res
