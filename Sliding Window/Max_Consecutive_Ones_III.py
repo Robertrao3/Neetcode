8596a68e60980fe13633a3535338742bcf763ebe
Robert Orfao <robertorfao7@gmail.com>
2025-05-13 10:16:16 -0400

    Max_Consecutive_Ones_III.py solution added to Sliding Window

---
+++ Sliding Window/Max_Consecutive_Ones_III.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:  
+        if not nums or not k:
+            return 0
+        
+        res = 0
+        l = 0
+        zeros = 0
+
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                zeros += 1
+            
+            if zeros <= k:
+                res = max(res, r - l + 1)
+            
+            while zeros > k:
+                if nums[l] == 0:
+                    zeros -= 1
+                l += 1
+        return res
